0e4ae2e6cff7631442fc0a59e408ec7ef3c69d4f
JackZanardo <SuperCombatWombat@protonmail.com>
2021-09-20 15:44:26 +1000

    Create Page model for reservations

--- BeanSceneProject/Models/Reservation/Create.cs
+++ BeanSceneProject/Models/Reservation/Create.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using BeanSceneProject.Data;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BeanSceneProject.Models.Reservation
 {
-    public class Create : PageModel
+    public class Create
     {
+        [Required(ErrorMessage ="Please select a time.")]
+        public DateTime Start { get; set; }
+        [Required(ErrorMessage ="Please select an area.")]
+        public Area Area { get; set; }
+        [Required(ErrorMessage ="Please enter number of guests.")]
+        public int CustomerNum { get; set; }
+        public string Notes { get; set; }
+
     }
 }
