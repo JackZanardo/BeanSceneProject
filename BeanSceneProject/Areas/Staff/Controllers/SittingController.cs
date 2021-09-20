1522ce23a30741afdbd4e7fc6df427c768147172
JackZanardo <SuperCombatWombat@protonmail.com>
2021-09-20 23:18:11 +1000

    Change to sitting controller

--- BeanSceneProject/Areas/Staff/Controllers/SittingController.cs
+++ BeanSceneProject/Areas/Staff/Controllers/SittingController.cs
@@ -1,6 +1,7 @@
 ﻿using BeanSceneProject.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,18 @@ namespace BeanSceneProject.Areas.Staff.Controllers
     public class SittingController : StaffAreaBaseController
     {
         public SittingController(ApplicationDbContext context) : base(context) { }
+
+        //GET: Sittings with sitting type and reservations
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Sittings
+                .Include(s => s.SittingType)
+                .Include(s => s.Reservations);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        
+
         
     }
 }
