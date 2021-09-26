793b221d9d439da5af5e7bd5b64bab7fcafa159c
JackZanardo <SuperCombatWombat@protonmail.com>
2021-09-26 21:39:31 +1000

    ReservationController added with create model. Changes to home index page.

---
+++ BeanSceneProject/Models/Reservation/Create.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeanSceneProject.Models.Reservation
+{
+    public class Create
+    {
+        public DateTime Start { get; set; }
+        public int CustomerNum { get; set; }
+        public int Duration { get; set; }
+        public string Notes { get; set; }
+
+        public int AreaId { get; set; }
+        public SelectList Areas { get; set; }
+
+        [Required(ErrorMessage = "First Name required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Phone Number required")]
+        public string MobileNumber { get; set; }
+    }
+}
