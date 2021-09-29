616140d3295620d48514ec29287891af4ed2f5a6
JackZanardo <SuperCombatWombat@protonmail.com>
2021-09-30 09:47:01 +1000

    Added Table reference to reservations

--- BeanSceneProject/Areas/Staff/Models/Sitting/Details.cs
+++ BeanSceneProject/Areas/Staff/Models/Sitting/Details.cs
@@ -7,5 +7,17 @@ namespace BeanSceneProject.Areas.Staff.Models.Sitting
 {
     public class Details
     {
+        public int Id { get; set; }
+        public DateTime Open { get; set; }
+        public DateTime Close { get; set; }
+        public bool IsClosed { get; set; }
+        public int Capacity { get; set; }
+        public int Heads { get; set; }
+        public int Reservations { get; set; }
+        public int BookedTables { get; set; }
+        public int RestuarantId { get; set; }
+        public int SittingTypeId { get; set; }
+
+
     }
 }
