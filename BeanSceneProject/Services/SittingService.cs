7549ccb68279225c14648bfeb8df72ac2b21dff1
JackZanardo <SuperCombatWombat@protonmail.com>
2021-10-10 03:53:48 +1100

    Added sitting sevice.

---
+++ BeanSceneProject/Services/SittingService.cs
@@ -0,0 +1,25 @@
+﻿using BeanSceneProject.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeanSceneProject.Services
+{
+    public class SittingService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SittingService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Sitting> GetSittingAsync(int id)
+        {
+            var sitting = await _context.Sittings.FirstOrDefaultAsync(s => s.Id == id);
+            return sitting;
+        }
+    }
+}
