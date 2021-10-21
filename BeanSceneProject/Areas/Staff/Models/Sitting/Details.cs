208f309bb8d632682c82d2e50e9a4eee4cc0f1dc
JackZanardo <SuperCombatWombat@protonmail.com>
2021-10-21 11:15:09 +1100

    Details page fixed

--- BeanSceneProject/Areas/Staff/Models/Sitting/Details.cs
+++ BeanSceneProject/Areas/Staff/Models/Sitting/Details.cs
@@ -9,22 +9,31 @@ namespace BeanSceneProject.Areas.Staff.Models.Sitting
     public class Details
     {
         public int Id { get; set; }
+
         [Display(Name ="Open Time")]
         public DateTime Open { get; set; }
+
         [Display(Name ="Close Time")]
         public DateTime Close { get; set; }
+
         [Display(Name ="Staus")]
         public bool IsClosed { get; set; }
+
         [Display(Name ="Capacity")]
         public int Capacity { get; set; }
+
         [Display(Name ="People Booked")]
         public int Heads { get; set; }
+
         [Display(Name ="Number of Reservations")]
         public int Reservations { get; set; }
+
         [Display(Name ="Booked Tables")]
-        public List<string> BookedTables { get; set; }
+        public string BookedTables { get; set; }
+
         [Display(Name ="Restuarant")]
         public string Restuarant { get; set; }
+
         [Display(Name ="Sitting Type")]
         public string SittingType { get; set; }
 
